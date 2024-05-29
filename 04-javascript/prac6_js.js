abfaaa54f369cbaa0ebf94522c321db44e0ce341
ymind14563 <ymind14563@gmail.com>
2024-05-29 19:21:40 +0900

    study: JS 함수 과제

---
+++ 04-javascript/prac6_js.js
@@ -0,0 +1,27 @@
+
+// 실습 1)
+// let num1 = prompt(`첫번째 숫자를 입력해주세요.`);
+// let num2 = prompt(`두번째 숫자를 입력해주세요.`);
+
+// function multifly(num1, num2) {
+//     return (Number(num1) * Number(num2));
+// }
+// console.log(multifly(num1, num2));
+
+// console.log(multifly(3, 7));
+// console.log(multifly(2, 2));
+
+
+// 실습 2)
+let a = prompt(`숫자를 입력해주세요.`);
+
+function square(a) {
+    return Number(a * a);
+}
+console.log(`입력한 숫자는 ${a} 입니다.`);
+console.log(square(a));
+
+
+// console.log(square(4));
+// console.log(square(11));
+// console.log(square(25));
