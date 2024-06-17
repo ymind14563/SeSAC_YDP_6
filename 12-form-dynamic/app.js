98096261950ada7c7b55b0e0f86b4e16148ac7b8
ymind14563 <ymind14563@gmail.com>
2024-06-17 16:16:15 +0900

    study: http 비동기 통신 - ajax

---
+++ 12-form-dynamic/app.js
@@ -0,0 +1,32 @@
+const express = require(`express`);
+const app = express()
+const PORT = 8080;
+
+app.set(`view engine`, `ejs`);
+app.set(`views`, `./views`);
+
+// body 내용이 보일 수 있게 해줌 (body-parser)
+app.use(express.urlencoded({extended : true}));
+app.use(express.json());
+
+app.get(`/`, (req, res) => {
+    res.render(`dynamic`, {title : `동적 폼을 배워보자`});
+})
+
+// ajax 요청 처리
+app.get(`/ajax`, (req, res) => {
+    // GET 방식이므로 브라우저에서 URL 쿼리까지 직접 입력해도 값을 확인 가능
+    // ex) http://localhost:8080/ajax?name=이름&gender=남자
+    console.log(req.query);
+    res.send(req.query);
+})
+
+app.post(`/ajax`, (req, res) => {
+    console.log(req.body);
+    res.send(req.body);
+})
+
+
+app.listen(PORT, () => {
+    console.log(`${PORT} 서버로 연결 성공`);
+})
