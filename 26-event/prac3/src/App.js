31973d132f217be3846f4223da6d9e60421e1882
ymind14563 <ymind14563@gmail.com>
2024-08-07 14:42:33 +0900

    study: 종합실습 풀이

---
+++ 26-event/prac3/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Prac from '../../prac3/src/Prac';
+
+function App() {
+  return (
+    <div className="App">
+      <Prac />
+    </div>
+  );
+}
+
+export default App;
---
+++ 26-event/prac4/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Input from './Input';
+import Prac from './Prac';
+import Result from './Result';
+import Select from './Select';
+
+function App() {
+  return (
+    <div className="App">
+      <Prac />
+    </div>
+  );
+}
+
+export default App;
