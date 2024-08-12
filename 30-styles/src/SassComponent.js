565bcec678adac4eeef81dbb5bfb748e9bab3bce
ymind14563 <ymind14563@gmail.com>
2024-08-12 19:04:03 +0900

    study: SassComponent

---
+++ 30-styles/src/SassComponent.js
@@ -0,0 +1,19 @@
+// npm i sass
+// rfc
+import React from 'react'
+import './styles/SassComponent.scss'
+
+export default function SassComponent() {
+  return (
+    <>
+    <div className='container'>
+        <div className='box red'>1</div>
+        <div className='box orange'>2</div>
+        <div className='box yellow'>3</div>
+    </div>
+    <div className='box yellow'></div>
+    <button className='btn'>Button</button>
+    <button className='btn-primary'>Button Primary</button>
+    </>
+  );
+}
