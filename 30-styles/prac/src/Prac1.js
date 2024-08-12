1d0ebd06e42b007760b77e28178954ceef0c24cc
ymind14563 <ymind14563@gmail.com>
2024-08-12 19:05:27 +0900

    study: React Styling 연습과제

---
+++ 30-styles/prac/src/Prac1.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import './styles/Prac1.scss'
+
+export default function Prac1() {
+  return (
+    <div className="larva">
+
+      <div className="body body1">
+        <div className="eye eye-white">
+          <div className="eye eye-black">
+          </div>
+        </div>
+      </div>
+
+      <div className="body body2"></div>
+      <div className="body body3"></div>
+      <div className="body body4"></div>
+      <div className="body body5"></div>
+
+    </div>
+  )
+}
